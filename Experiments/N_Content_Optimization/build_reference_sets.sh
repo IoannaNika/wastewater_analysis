eb25590a135929407b88d819ea8036e535bb811b
Ioanna Nika <nana11892@gmail.com>
2022-07-12 23:18:46 +0300

    adjust hpc settings

--- Experiments/N_Content_Optimization/build_reference_sets.sh
+++ Experiments/N_Content_Optimization/build_reference_sets.sh
@@ -35,29 +35,29 @@ for n_content in 0.0 0.001 0.01 0.1; do \
             python ../../pipeline/pipeline/preprocess_references.py -m $metadata -f $sequences --seed 0 -o reference_sets/$n_content/$folder_name --startdate $start_date --enddate $end_date --state $location --max_N_content $n_content
         fi 
 
-        # # calculate within lineage variation
-        # bash ../../pipeline/pipeline/call_variants.sh reference_sets/$n_content/$folder_name /tudelft.net/staff-umbrella/SARSCoV2Wastewater/inika/wastewater_analysis/data/Original_SARS-CoV-2_sequence/SARS-CoV-2-NC_045513.fa
-        # # select samples
-        # python ../../pipeline/pipeline/select_samples.py -m $metadata -f $sequences -o reference_sets/$n_content/$folder_name --vcf reference_sets/$n_content/$folder_name/*_merged.vcf.gz --freq reference_sets/$n_content/$folder_name/*_merged.frq
+        # calculate within lineage variation
+        bash ../../pipeline/pipeline/call_variants.sh reference_sets/$n_content/$folder_name /tudelft.net/staff-umbrella/SARSCoV2Wastewater/inika/wastewater_analysis/data/Original_SARS-CoV-2_sequence/SARS-CoV-2-NC_045513.fa
+        # select samples
+        python ../../pipeline/pipeline/select_samples.py -m $metadata -f $sequences -o reference_sets/$n_content/$folder_name --vcf reference_sets/$n_content/$folder_name/*_merged.vcf.gz --freq reference_sets/$n_content/$folder_name/*_merged.frq
 
-        # #build kallisto index
-        # kallisto index -i reference_sets/$n_content/$folder_name/sequences.kallisto_idx  reference_sets/$n_content/$folder_name/sequences.fasta
+        #build kallisto index
+        kallisto index -i reference_sets/$n_content/$folder_name/sequences.kallisto_idx  reference_sets/$n_content/$folder_name/sequences.fasta
 
-        # # remove everything except the metadata file, the fasta file, the lineage contents file & the kallisto index
+        # remove everything except the metadata file, the fasta file, the lineage contents file & the kallisto index
 
-        # for file in reference_sets/$n_content/$folder_name/*; do \
+        for file in reference_sets/$n_content/$folder_name/*; do \
 
-        #     if [ -d "$file" ] ; then
-        #         echo "$file is a directory and it will be removed";
-        #         rm -r $file
-        #     fi
+            if [ -d "$file" ] ; then
+                echo "$file is a directory and it will be removed";
+                rm -r $file
+            fi
 
-        #     if ! [[ "$file"  =~ ^(reference_sets/$n_content/$folder_name/metadata.tsv|reference_sets/$n_content/$folder_name/sequences.kallisto_idx|reference_sets/$n_content/$folder_name/sequences.fasta|reference_sets/$n_content/$folder_name/lineages.txt)$ ]]; 
-        #     then 
-        #         echo "$file is removed"
-        #         rm $file
-        #     fi
-        # done
+            if ! [[ "$file"  =~ ^(reference_sets/$n_content/$folder_name/metadata.tsv|reference_sets/$n_content/$folder_name/sequences.kallisto_idx|reference_sets/$n_content/$folder_name/sequences.fasta|reference_sets/$n_content/$folder_name/lineages.txt)$ ]]; 
+            then 
+                echo "$file is removed"
+                rm $file
+            fi
+        done
 
     done
 done

7ee75c284b3fcfb0b5c9c1fbc2c253839c24da89
Ioanna Nika <nana11892@gmail.com>
2022-07-12 21:24:39 +0300

    fix string comparison

--- Experiments/N_Content_Optimization/build_reference_sets.sh
+++ Experiments/N_Content_Optimization/build_reference_sets.sh
@@ -22,7 +22,7 @@ for n_content in 0.0 0.001 0.01 0.1; do \
         if [[ "$location_type" =~ "all" ]]; then
             # preprocess references
             python ../../pipeline/pipeline/preprocess_references.py -m $metadata -f $sequences --seed 0 -o reference_sets/$n_content/$folder_name --startdate $start_date --enddate $end_date --max_N_content $n_content
-        elif [[ "$location_type"=~== "continent" ]]; then
+        elif [[ "$location_type"=~ "continent" ]]; then
             # preprocess references
             python ../../pipeline/pipeline/preprocess_references.py -m $metadata -f $sequences --seed 0 -o reference_sets/$n_content/$folder_name --startdate $start_date --enddate $end_date --continent $location --max_N_content $n_content
         
@@ -30,7 +30,7 @@ for n_content in 0.0 0.001 0.01 0.1; do \
             # preprocess references
             python ../../pipeline/pipeline/preprocess_references.py -m $metadata -f $sequences --seed 0 -o reference_sets/$n_content/$folder_name --startdate $start_date --enddate $end_date --country $location --max_N_content $n_content
 
-        else [[ "$location_type"=~== "state" ]]; then
+        else [[ "$location_type" =~ "state" ]]; then
             # preprocess references
             python ../../pipeline/pipeline/preprocess_references.py -m $metadata -f $sequences --seed 0 -o reference_sets/$n_content/$folder_name --startdate $start_date --enddate $end_date --state $location --max_N_content $n_content
         fi 

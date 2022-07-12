867d01b43672234c147654a497a41c27f8ce19f6
Ioanna Nika <nana11892@gmail.com>
2022-07-12 14:47:26 +0300

    fix script for reference set creation -enddate typo

--- Experiments/N_Content_Optimization/build_reference_sets.sh
+++ Experiments/N_Content_Optimization/build_reference_sets.sh
@@ -21,10 +21,10 @@ for n_content in 0.0 0.001 0.01 0.1; do \
 
         if $location_type == "all"; then
             # preprocess references
-            python ../../pipeline/pipeline/preprocess_references.py -m $metadata -f $sequences --seed 0 -o reference_sets/$n_content/$folder_name --startdate $start_date --enddate $enddate --max_N_content $n_content
+            python ../../pipeline/pipeline/preprocess_references.py -m $metadata -f $sequences --seed 0 -o reference_sets/$n_content/$folder_name --startdate $start_date --enddate $end_date --max_N_content $n_content
         else
             # preprocess references
-            python ../../pipeline/pipeline/preprocess_references.py -m $metadata -f $sequences --seed 0 -o reference_sets/$n_content/$folder_name --startdate $start_date --enddate $enddate --$location_type $location --max_N_content $n_content
+            python ../../pipeline/pipeline/preprocess_references.py -m $metadata -f $sequences --seed 0 -o reference_sets/$n_content/$folder_name --startdate $start_date --enddate $end_date --$location_type $location --max_N_content $n_content
         fi
         # calculate within lineage variation
         bash ../../pipeline/pipeline/call_variants.sh reference_sets/$n_content/$folder_name /tudelft.net/staff-umbrella/SARSCoV2Wastewater/inika/wastewater_analysis/data/Original_SARS-CoV-2_sequence/SARS-CoV-2-NC_045513.fa

4b8414696ca0e24cddf771dfa09b870b0eb768cd
Ioanna Nika <nana11892@gmail.com>
2022-07-16 13:15:58 +0300

    fix date in script for ww simulation

--- Experiments/N_Content_Optimization/produce_ww.sh
+++ Experiments/N_Content_Optimization/produce_ww.sh
@@ -3,7 +3,7 @@ data="Connecticut"
 mkdir -p benchmarks/$data
 metadata="../../../../GISAID/gisaid_2022_06_12/metadata.tsv"
 sequences="../../../../GISAID/gisaid_2022_06_12/sequences.fasta"
-python ../../benchmarking/create_benchmarks.py --voc_perc 1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90,100 -m $metadata -fr $sequences -fv ../../data/N_Content_Optimization/B.1.1.7_sequence.fasta -o benchmarks/${data} --total_cov 100 --state Connecticut --date 2021-04-01
+python ../../benchmarking/create_benchmarks.py --voc_perc 1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90,100 -m $metadata -fr $sequences -fv ../../data/N_Content_Optimization/B.1.1.7_sequence.fasta -o benchmarks/${data} --total_cov 100 -s ${data} -d 2021-04-30
 
 # remove intermidiate files
 for file in benchmarks/$data/*; do \
